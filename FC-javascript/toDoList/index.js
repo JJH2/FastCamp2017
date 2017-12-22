d043f7806f722f4644156ec9bae6423f579c2bf4
Jang Jaehoon <idlinlee12@gmail.com>
2017-12-22 16:51:44 +0900

    update

--- FC-javascript/toDoList/index.js
+++ FC-javascript/toDoList/index.js
@@ -5,6 +5,23 @@ const listEl = document.querySelector('.todo-list');
 addButtonEl.addEventListener('click', e => {
     const itemEl = document.createElement('div');
     itemEl.textContent = inputEl.value;
+
     listEl.appendChild(itemEl);
     inputEl.value = '';
+
+    itemEl.addEventListener('click', e => {
+        if (itemEl.classList.contains('complete')) {
+            itemEl.classList.remove('complete');
+        } else {
+            itemEl.classList.add('complete');
+        }
+    });
+
+    const removeBtnEl = document.createElement('div');
+    itemEl.appendChild(removeBtnEl);
+    removeBtnEl.textContent = 'X'
+
+    removeBtnEl.addEventListener('click', e => {
+        listEl.removeChild(itemEl);
+    })
 })
